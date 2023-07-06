66a688f2d4dae0f2b1284c4244e43e7e68c5f5c1
kittinan-k <kittinan-k@kyocera.co.th>
2023-07-06 16:23:43 +0700

    ok

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
   imports: [IonicModule, ExploreContainerComponent],
+  providers: [BarcodeScanner]
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(
+    private barcodeScanner: BarcodeScanner
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+  openCamera() {
+    this.barcodeScanner.scan().then((barcodeData: any) => {
+      console.log('Barcode data', barcodeData);
+    }).catch((err: any) => {
+      console.log('Error', err);
+    });
+  }
 }
